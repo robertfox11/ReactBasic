4ce51533141d5f1c66147e06b3a1c0da0f0589c2
robert Salazar <11rsahome@gmail.com>
2020-05-19 17:22:19 +0200

    seleccionando el prodcuto pasando con el parametro

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 const Product = ({ product }) => {
   const { name, price, id } = product;
+  const selectProducto = (id) => {
+    console.log("comprando....", id);
+  };
   return (
     <div>
       <h2>{name}</h2>
       <h3>€{price}</h3>
+      {/* //creamos la funciont selectionado el producto */}
+      <button type="button" onClick={() => selectProducto(id)}>
+        Buy
+      </button>
     </div>
   );
 };
